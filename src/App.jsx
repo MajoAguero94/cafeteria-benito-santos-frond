813854c3d3f19aabc22d0ae627f5e253eb0d5188
MajoA <majoaguero20@hotmail.comgit config --global user.name MajoAgit commit -m 'primer commit del proyecto de la panaderia''git config --global user.email majoaguero20@hotmail.com>
2022-10-18 18:32:23 -0300

    se agregaron route de inicio, administrador y error404

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Administrador from './components/views/Administrador';
+import Inicio from './components/views/Inicio';
+import Error404 from './components/views/Error404';
 function App() {
   return (
-    <h1 className='display-1'>Hola</h1>
+  //administramos las rutas
+  <BrowserRouter>
+  <Routes>
+    <Route exact path='/' element= {<Inicio></Inicio>} ></Route>
+    <Route exact path='/administrador' element={<Administrador></Administrador>}></Route>
+    <Route path='*' element={<Error404></Error404>} ></Route>
+  </Routes>
+  
+  
+  
+  </BrowserRouter>
   );
 }
 
---
+++ src/components/views/Error404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error404 = () => {
+    return (
+        <div>
+            <h1>Pagina de error</h1>
+        </div>
+    );
+};
+
+export default Error404;
